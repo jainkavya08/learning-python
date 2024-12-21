af0c2861799ea688db6fdf66234d327a371477bd
jainkavya08 <jkavya865@gmail.com>
2024-12-21 10:26:25 +0530

    new commits

---
+++ Week 0/03_namedparameters.py
@@ -0,0 +1,15 @@
+# Ask user for their name
+name = input ("What's your name ? ")
+
+# changing the end 
+print ("Hello, ", end = "" )
+print (name)
+
+######
+
+# Ask user for their name
+name = input ("What's your name ? ")
+
+# changing the sep
+print ("Hello,", name, sep = "" )
+

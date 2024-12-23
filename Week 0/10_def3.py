fe930aaa37dc58059c47c346ac92e40b3464485e
jainkavya08 <jkavya865@gmail.com>
2024-12-23 18:25:14 +0530

    commit 05

---
+++ Week 0/10_def3.py
@@ -0,0 +1,10 @@
+# Create our own function
+def hello (to ="world"):
+    print ("hello ,",to)
+
+# Output using our own function
+name = input ("What's your name ? ")
+hello (name)
+
+# Output without passing the expected arguments
+hello()

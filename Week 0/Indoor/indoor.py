18dc259c34aff71cb202df6ce1fcd55d11f762ce
jainkavya08 <jkavya865@gmail.com>
2024-12-25 12:25:49 +0530

    Delete Week 0/Indoor directory

--- Week 0/Indoor/indoor.py
+++
@@ -1,5 +0,0 @@
-# Asking user for the input 
-indoor = input ("Enter a sentence in UPPERCASE : ").casefold()      #you can use lower() too
-
-# Output for the user's input as lowercase 
-print (f"Here is lowercased version using str : {indoor}")  

0781c0561ff7d024bacc9a3f085a2c59cc3440a9
jainkavya08 <jkavya865@gmail.com>
2024-12-25 12:23:40 +0530

    tue streak

---
+++ Week 0/Indoor/indoor.py
@@ -0,0 +1,5 @@
+# Asking user for the input 
+indoor = input ("Enter a sentence in UPPERCASE : ").casefold()      #you can use lower() too
+
+# Output for the user's input as lowercase 
+print (f"Here is lowercased version using str : {indoor}")  

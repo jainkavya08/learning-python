5220b3d83a66bcadd5cbe09590d4ec971c1fcc95
jainkavya08 <jkavya865@gmail.com>
2024-12-28 17:01:26 +0530

    Week 01

---
+++ Week 1/02_compare_elif.py
@@ -0,0 +1,15 @@
+# Why was this code better?
+# Ans --> Because the code stops if it gets true right away and doesn't take system memory
+
+x = int(input ("What's x ? "))
+y= int(input ("What's y ? "))
+
+if x < y:    #Boolean Expression
+    print ("x is less than y ")
+elif x > y:
+    print ("x is greater than y")
+elif x == y:
+    print ("x equals y ")
+
+
+    
---
+++ Week 1/03_compare_else.py
@@ -0,0 +1,16 @@
+# Why was this else at the end?
+# Ans --> If both the above condition are equated to false then eventually the last statement is conditioned to be true 
+# that's why else is used and boolean expression needs to be used 
+
+x = int(input ("What's x ? "))
+y= int(input ("What's y ? "))
+
+if x < y:    #Boolean Expression
+    print ("x is less than y ")
+elif x > y:
+    print ("x is greater than y")
+else:
+    print ("x equals y ")
+
+
+    

9f3792049b96560dfd5f47265e19e46ee6fcb0fe
jainkavya08 <jkavya865@gmail.com>
2024-12-31 14:26:44 +0530

    shorts boolean exp

--- Week 1/Shorts/reccomendation.py
+++ Week 1/Shorts/reccomendation.py
@@ -1,21 +1,24 @@
 def main():
     difficulty = input("Difficult or Casual? ")
+    if not (difficulty == "Difficult" or difficulty=="Casual") :
+        print ("Enter a valid difficulty")
+        return
+    
     players = input("Multiplayer or Single-player? ")
+    if not (players =="Multiplayer" or players =="Single-player"):
+        print("Enter a valid number of players")
 
     if difficulty == "Difficult":
-        if players == "Multiplayer":
-            reccomend("Poker")
-        elif players == "Single-player":
-            reccomend("Klondike")
-        else:
-            print("Enter a valid number of players ")
-    else : 
         if players =="Multiplayer":
-            reccomend("Heearts")
+            reccomend("Poker")
         elif players =="Single-player":
-            reccomend("Clock")
+            reccomend("Klondike")
+    elif difficulty =="Casual":
+        if players =="Multiplayer":
+            reccomend("Hearts")
         else:
-            print("Enter a valid number of players ")
+            reccomend("Clock")
+
 
 def reccomend(game):
     print("You might like " , game)

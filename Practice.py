f023705d1134eebd75c96aea9e37752b547cce23
jainkavya08 <jkavya865@gmail.com>
2024-12-31 14:58:04 +0530

    anki

--- Practice.py
+++ Practice.py
@@ -1,5 +1,12 @@
-x = int(input("Enter x : "))
-y = int(input("Enter y : "))
+def hello(to="World"):
+    # Print a greeting message that includes the name passed to the function
+    print("Hello,", to)
 
-print (f"sum : {x + y}")
+# Prompt the user for their name and store the input in the variable 'name'
+name = input("What's your name? ")
 
+# Call the 'hello' function without any arguments, which will use the default value "World"
+hello()
+
+# Call the 'hello' function, passing the user's name as an argument
+hello(name)
